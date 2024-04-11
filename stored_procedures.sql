f7d4522ed0739def589fd1a9fdb5d4481d678a58
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-11 18:41:26 -0400

    agregar stored procedure para obtener materias abiertas

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,11 +1,15 @@
 CREATE OR REPLACE FUNCTION obtenerMateriasAbiertas() 
-returns table (nombre_materia,nombre_docente,nivel,grupo) AS $func$
+returns table (nombre_materia VARCHAR,nombre_docente TEXT,nivel CHAR,grupo CHAR) AS $func$
 begin
 
 return query
 
-select id_user,username from Users where id_user IN(
-    select id_user from docente
-) ORDER by username;
+SELECT m.nombre_materia,
+CONCAT(u.nombre,' ',u.apellido_p,' ',u.apellido_m) AS nombre_docente,
+m.nivel, g.grupo FROM
+(
+    SELECT grupo_materia.grupo, grupo_materia.id_docente,grupo_materia.id_materia FROM grupo_materia WHERE estado = 'ABIERTA'
+) g, materias as m, usuarios as u
+WHERE u.id_user = g.id_docente AND g.id_materia = m.id_materia;
     
 end; $func$ LANGUAGE plpgsql;
