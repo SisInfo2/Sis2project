fa0bef73bec5a4ebc16b2d9d6941a16bffbf1e61
JGest <201905558@est.umss.edu>
2024-04-11 18:27:14 -0400

    se ha creado el JFrame pra estudiante (para que pueda visualizar sus observaciones)

--- PlataformaEducativa/src/observacionesTrabajoDocente.java
+++ PlataformaEducativa/src/observacionesTrabajoDocente.java
@@ -7,12 +7,12 @@
  *
  * @author WorkStation
  */
-public class observacionesTrabajo extends javax.swing.JFrame {
+public class observacionesTrabajoDocente extends javax.swing.JFrame {
 
     /**
      * Creates new form observacionesTrabajo
      */
-    public observacionesTrabajo() {
+    public observacionesTrabajoDocente() {
         initComponents();
     }
 
@@ -127,20 +127,21 @@ public class observacionesTrabajo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(observacionesTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(observacionesTrabajoDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(observacionesTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(observacionesTrabajoDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(observacionesTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(observacionesTrabajoDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(observacionesTrabajo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(observacionesTrabajoDocente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new observacionesTrabajo().setVisible(true);
+                new observacionesTrabajoDocente().setVisible(true);
             }
         });
     }
