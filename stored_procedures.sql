8bc6a66c2f728dbb4d91e12e0488bf0ca42a9d01
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-12 14:48:27 -0400

    agregar version basica de la funcion inscribir estudiante

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -14,3 +14,14 @@ WHERE u.id_user = g.id_docente AND g.id_materia = m.id_materia ORDER BY m.nivel;
     
 end; $func$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION inscribirse(id_est INT,id_grupo INT)
+RETURNS TEXT AS $$
+DECLARE
+response TEXT := 'success';
+begin
+
+INSERT INTO clase (id_estudiante,id_grupo_materia) VALUES (id_est,id_grupo);
+
+return response;
+    
+end; $$ LANGUAGE plpgsql;
