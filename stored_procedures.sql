454a65482662b3cd52b84dba6a28e68fe74951b7
Kevin Huayllas <KevinOmega01@gmail.com>
2024-04-13 16:22:09 -0400

    agregar obtener materias abiertas

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION obtenerMateriasAbiertas(INT id_est) 
+CREATE OR REPLACE FUNCTION obtenerMateriasAbiertas(id_est INT) 
 returns table (nombre_materia VARCHAR,nombre_docente TEXT,nivel CHAR,grupo CHAR) AS $func$
 begin
 
@@ -10,30 +10,32 @@ m.nivel, g.grupo FROM
 (
     SELECT grupo_materia.grupo, grupo_materia.id_docente,grupo_materia.id_materia 
     FROM grupo_materia 
-    WHERE periodo_academico = (
-        SELECT id_periodo FROM periodo_academico WHERE activo = True AND
+    WHERE 
         id_materia IN (
             SELECT id_materia FROM materias WHERE 
-            id_materia IN (
+            (id_materia  NOT IN (
                 SELECT id_materia FROM dependencia_materia  
             ) OR 
             id_materia IN (
                 SELECT id_materia FROM dependencia_materia
                 WHERE id_dependencia IN (
-                    SELECT id_materia FROM grupo_materia WHERE 
-                    id_grupo_materia IN(
-                        SELECT id_grupo_materia FROM clase 
-                        WHERE estado = 'APROBADO' AND
-                        id_estudiante=id_est
-                    )
+                    SELECT id_materia FROM obtenerMateriasAprobadas(2)
                 )
+            ))
+			AND id_materia NOT IN(
+				SELECT id_materia FROM obtenerMateriasEstudiante(2,'APROBADO')
+			) AND id_materia NOT IN (
+                SELECT id_materia from obtenerMateriasEstudiante(2,'CURSANDO')
             )
-        )
+        ) AND periodo_academico = (
+        SELECT id_periodo FROM periodo_academico WHERE activo = True 
     )
 ) g, materias as m, usuarios as u
 WHERE u.id_user = g.id_docente AND g.id_materia = m.id_materia ORDER BY m.nivel;
     
 end; $func$ LANGUAGE plpgsql;
+
+
 
 CREATE OR REPLACE FUNCTION inscribirse(id_est INT,id_grupo INT)
 RETURNS TEXT AS $$
@@ -48,8 +50,7 @@ return response;
 end; $$ LANGUAGE plpgsql;
 
 
-
-CREATE OR REPLACE FUNCTION obtenerMateriasAprobadas(id_est INT) 
+CREATE OR REPLACE FUNCTION obtenerMateriasEstudiante(id_est INT, tipo TEXT) 
 returns table (id_materia INT,nombre_materia VARCHAR,nivel CHAR,promedio INT) AS $func$
 begin
 
@@ -61,7 +62,7 @@ SELECT g.id_materia id_materia,one.promedio
 FROM (
     SELECT id_grupo_materia,nota_final AS promedio
 	FROM clase 
-    WHERE estado = 'APROBADO' AND
+    WHERE estado = tipo::varchar AND
     id_estudiante=id_est
 )one, grupo_materia as g
 WHERE one.id_grupo_materia = g.id_grupo_materia
