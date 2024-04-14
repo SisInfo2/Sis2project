39ee801bddd60b1e1116c8d37ebfd4fe18c542f6
Nicolaspm <nicknoirpm@gmail.com>
2024-04-14 12:44:51 -0300

    MetodoModificarPDF1

---
+++ MetodoModificarPDF1/EntregarTabajo/src/main/java/DAO/PdfDAO.java
@@ -0,0 +1,102 @@
+package DAO;
+
+import Conexion.Conectar;
+import VO.PdfVO;
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+/**
+ *
+ * @author Leandro Nicolas Perez Maldonado
+ */
+public class PdfDAO {
+
+    /*Metodo listar*/
+    public ArrayList<PdfVO> Listar_PdfVO() {
+        ArrayList<PdfVO> list = new ArrayList<PdfVO>();
+        Conectar conec = new Conectar();
+        String sql = "SELECT * FROM pdf;";
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+        try {
+            ps = conec.getConnection().prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                PdfVO vo = new PdfVO();
+                vo.setCodigopdf(rs.getInt(1));
+                vo.setNombrepdf(rs.getString(2));
+                vo.setArchivopdf(rs.getBytes(3));
+                list.add(vo);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                ps.close();
+                rs.close();
+                conec.desconectar();
+            } catch (Exception ex) {
+            }
+        }
+        return list;
+    }
+
+
+    /*Metodo agregar*/
+    public void Agregar_PdfVO(PdfVO vo) {
+        Conectar conec = new Conectar();
+        String sql = "INSERT INTO pdf (codigopdf, nombrepdf, archivopdf) VALUES(?, ?, ?);";
+        PreparedStatement ps = null;
+        try {
+            ps = conec.getConnection().prepareStatement(sql);
+            ps.setInt(1, vo.getCodigopdf());
+            ps.setString(2, vo.getNombrepdf());
+            ps.setBytes(3, vo.getArchivopdf());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                ps.close();
+                conec.desconectar();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    /*Metodo Modificar*/
+    public void Modificar_PdfVO(PdfVO vo) {
+        Conectar conec = new Conectar();
+        String sql = "UPDATE pdf SET nombrepdf = ?, archivopdf = ? WHERE codigopdf = ?;";
+        PreparedStatement ps = null;
+        try {
+            ps = conec.getConnection().prepareStatement(sql);
+            ps.setString(1, vo.getNombrepdf());
+            ps.setBytes(2, vo.getArchivopdf());
+            ps.setInt(3, vo.getCodigopdf());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                ps.close();
+                conec.desconectar();
+            } catch (Exception ex) {
+            }
+        }
+    }    
+
+}
+
